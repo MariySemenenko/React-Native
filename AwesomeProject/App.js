19858ea6dff0c6c4974a6315a72dc2e8d94b3e6d
MariySemenenko <mashyni92@gmail.com>
2023-07-18 19:06:07 +0300

    create expro

---
+++ AwesomeProject/App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        style={styles.image}
+        source={require('./images')}
+        >
+      </ImageBackground>
+      <Text style={{color: 'green', fontSize: 30}}>I love my sun Sergio :) </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center'
+  }
+});
